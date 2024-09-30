4246c9f33ca41a2ca2feda8b4b782159c9f95857
Tyler Schunk <141867422+tyler-schunk@users.noreply.github.com>
2024-09-30 14:30:05 -0500

    added firefox-esr option

--- attackbox-setup.sh
+++ attackbox-setup.sh
@@ -26,7 +26,8 @@ if ! command -v firefox &> /dev/null; then
   sudo install -d -m 0755 /etc/apt/keyrings;
   wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null;
   echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-  sudo apt update && sudo apt install firefox -y;
+  sudo apt update
+  sudo apt install firefox firefox-esr -y;
   
   # Find the Firefox executable path
   firefox_path=$(which firefox)
